531906abe7e7e1a2fcacd95c201e52a18cbac3c1
Shabaz <shabaz@Shabazs-iMac.local>
2018-05-12 23:53:08 -0400

    Added shortcut for Developer Console Changed shortcut keys (pages now alt+shift+p, and classes now alt+shift+c) Added ability to open in new tab Refactored code

--- background.js
+++ background.js
@@ -1,15 +1,22 @@
 var currentSfdcTabBaseUrl;
 
-// var sfdcUrlExtensions = {
-//   setup: '/setup/forcecomHomepage.apexp?setupid=ForceCom&retURL=%2Fhome%2Fhome.jsp',
-//   pages: '/apexpages/setup/listApexPage.apexp?retURL=%2Fui%2Fsetup%2FSetup%3Fsetupid%3DDevToolsIntegrate&setupid=ApexPages',
-//   classes: '/01p?retURL=%2Fui%2Fsetup%2FSetup%3Fsetupid%3DDevToolsIntegrate&setupid=ApexClasses'
-// };
-
 var sfdcUrlExtensions = {
-  goToSfdcSetup: '/setup/forcecomHomepage.apexp?setupid=ForceCom&retURL=%2Fhome%2Fhome.jsp',
-  goToSfdcPages: '/apexpages/setup/listApexPage.apexp?retURL=%2Fui%2Fsetup%2FSetup%3Fsetupid%3DDevToolsIntegrate&setupid=ApexPages',
-  goToSfdcClasses: '/01p?retURL=%2Fui%2Fsetup%2FSetup%3Fsetupid%3DDevToolsIntegrate&setupid=ApexClasses'
+  goToSfdcSetup: {
+    url: '/setup/forcecomHomepage.apexp?setupid=ForceCom&retURL=%2Fhome%2Fhome.jsp',
+    newTab: false
+  },
+  goToSfdcPages: {
+    url: '/apexpages/setup/listApexPage.apexp?retURL=%2Fui%2Fsetup%2FSetup%3Fsetupid%3DDevToolsIntegrate&setupid=ApexPages',
+    newTab: false
+  },
+  goToSfdcClasses: {
+    url: '/01p?retURL=%2Fui%2Fsetup%2FSetup%3Fsetupid%3DDevToolsIntegrate&setupid=ApexClasses',
+    newTab: false
+  },
+  goToSfdcDevConsole: {
+    url: '/_ui/common/apex/debug/ApexCSIPage',
+    newTab: true
+  }
 };
 
 
@@ -22,26 +29,42 @@ chrome.runtime.onMessage.addListener(
 
 // Listen for goToSfdcSetup hotkey defined in manifest.json
 chrome.commands.onCommand.addListener(
-  function(command){
+  function(command, somethingelse){
     var commandExtension = sfdcUrlExtensions[command];
-    updateCurrentTab(commandExtension);
+
+    // Validate command exists in sfdcUrlExtensions
+    if(!commandExtension){
+      alert('Unknown command ' + commandExtension + '. Add to sfdcUrlExtensions.');
+      return;
+    }
+
+    goToUrl(commandExtension.url, commandExtension.newTab);
   }
 );
 
-function updateCurrentTab(urlExtension){
+function goToUrl(urlExtension, newTab){
   // Validate that the currentTabUrl exists
   if(!currentSfdcTabBaseUrl){
     alert('Missing current tab url? ' + currentSfdcTabBaseUrl);
     return;
   }
 
-  // Update tab url
-  chrome.tabs.query(
-    {active: true, currentWindow: true},
-    function(arrayOfTabs){
-      var activeTab = arrayOfTabs[0];
-      var activeTabId = activeTab.id;
-      chrome.tabs.update(activeTabId, {url: currentSfdcTabBaseUrl + urlExtension});
-    }
-  );
+  // // New URL
+  var newURL = currentSfdcTabBaseUrl + urlExtension;
+
+  // // Open URL in new tab
+  if(newTab){
+    window.open(newURL, '_blank');
+  }
+  // Open URL in current tab
+  else{
+    chrome.tabs.query(
+      {active: true, currentWindow: true},
+      function(arrayOfTabs){
+        var activeTab = arrayOfTabs[0];
+        var activeTabId = activeTab.id;
+        chrome.tabs.update(activeTabId, {url: currentSfdcTabBaseUrl + urlExtension});
+      }
+    );
+  }
 }
