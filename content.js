44bd6ee4652a23e5da33dace752295d28fa076ad
Shabaz Farooq <shabaz6@gmail.com>
2025-09-21 16:40:54 -0500

    Not sure of what changes are in this commit

--- content.js
+++ content.js
@@ -11,12 +11,12 @@ switch(window.location.pathname) {
   // Lightning Setup page
   // Do: Set focus on search bar
   case '/lightning/setup/SetupOneHome/home':
-    setTimeout(()=> document.getElementsByClassName('filter-box input')[0].focus(), 2000);
+    // setTimeout(()=> document.getElementsByClassName('filter-box input')[0].focus(), 2000);
     break;
 
   // Lightning Object Manager page
   // Do: Set focus on search bar
   case '/lightning/setup/ObjectManager/home':
-    setTimeout(()=> document.getElementById('globalQuickfind').focus(), 2000);
+    // setTimeout(()=> document.getElementById('globalQuickfind').focus(), 2000);
     break;
 }
